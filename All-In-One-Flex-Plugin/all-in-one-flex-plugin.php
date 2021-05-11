662847257a435b78a221f2be44318a8eded58e6e
teunrompa <47944450+teunrompa@users.noreply.github.com>
2021-05-11 15:34:01 +0200

    Version 0.8
    This is a experimental working version.

--- All-In-One-Flex-Plugin/all-in-one-flex-plugin.php
+++ All-In-One-Flex-Plugin/all-in-one-flex-plugin.php
@@ -23,85 +23,10 @@ add_action( 'init', 'my_load_module_examples' );
 
 
 
-// if ( ! function_exists( 'PostCreator' ) ) {
-
-// 	function PostCreator(
-// 		$name      = 'AUTO POST',
-// 		$type      = 'post',
-// 		$content   = 'DUMMY CONTENT',
-// 		$category  = array(1,2),
-// 		$template  = NULL,
-// 		$author_id = '1',
-// 		$status    = 'publish'
-// 	) {
-
-// 		define( POST_NAME, $name );
-// 		define( POST_TYPE, $type );
-// 		define( POST_CONTENT, $content );
-// 		define( POST_CATEGORY, $category );
-// 		define( POST_TEMPLATE, '' );
-// 		define( POST_AUTH_ID, $author_id );
-// 		define( POST_STATUS, $status );
-
-// 		if ( $type == 'page' ) {
-// 			$post      = get_page_by_title( POST_NAME, 'OBJECT', $type );
-// 			$post_id   = $post->ID;
-// 			$post_data = get_page( $post_id );
-// 			define( POST_TEMPLATE, $template );
-// 		} else {
-// 			$post      = get_page_by_title( POST_NAME, 'OBJECT', $type );
-// 			$post_id   = $post->ID;
-// 			$post_data = get_post( $post_id );
-// 		}
-
-// 		function hbt_create_post() {
-// 			$post_data = array(
-// 				'post_title'    => wp_strip_all_tags( POST_NAME ),
-// 				'post_content'  => POST_CONTENT,
-// 				'post_status'   => POST_STATUS,
-// 				'post_type'     => POST_TYPE,
-// 				'post_author'   => POST_AUTH_ID,
-// 				'post_category' => POST_CATEGORY,
-// 				'page_template' => POST_TEMPLATE
-// 			);
-// 			wp_insert_post( $post_data, $error_obj );
-// 		}
-
-// 		if ( ! isset( $post ) ) {
-// 			add_action( 'admin_init', 'hbt_create_post' ,'', '1','publish');
-// 			return $error_obj;
-// 		}
-
-// 	}
-// }
-// PostCreator(
-// 	'flexcontent',
-// 	'acf-field-group',
-// 	'test',
-// 	array( 1, 2 ) ,
-// 	'',
-// 	'1',
-//     'publish'
-// );
-// $post = get_page_by_title('flexcontent', 'OBJECT', 'acf-field-group');
-// $post_id = $post->ID;
-// echo 'owidajoiawjdoiawjd';
-
-// var_dump($post_id);
-
-
-
-// function registerCustomField(){
-//     acf_add_local_field(array(
-//         'key' => 'field_1',
-//         'label' => 'Sub Title',
-//         'name' => 'sub_title',
-//         'type' => 'text',
-//         'parent' => 'group_6090feea09e0e'
-//     ));
-// }
 
 require_once 'inc/settings/settings.php';
 
-add_action('acf/init','registerCustomField');
+
+
+add_action('acf/init','return_location_settings');
 ?>
--- All-In-One-Flex-Plugin/inc/settings/settings.php
+++ All-In-One-Flex-Plugin/inc/settings/settings.php
@@ -117,7 +117,6 @@ function flex_settings_input_field_callback(){
 /**
  * select settings field
  */
-
 function flex_settings_checkbox_field_callback(){
     
     
@@ -125,57 +124,288 @@ function flex_settings_checkbox_field_callback(){
         'public'   => true,
         '_builtin' => false
     );
-    $output = 'names'; // names or objects, note names is the default
+    $output = 'objects'; // names or objects, note names is the default
     $operator = 'and'; // 'and' or 'or'
     $post_types = get_post_types( $args, $output, $operator ); 
     $i = 0;
-    var_dump($post_types);
+
+    $flex_checkbox_field = get_option('flex_settings_checkbox_field');
+
     foreach ( $post_types  as $post_type ) {
-        echo '<p>Custom Post Type name: ' . $post_type->name . "<br />\n";
-        echo 'Single name: ' . $post_type->labels->singular_name . "<br />\n";
-        echo 'Menu icon URL: ' . $post_type->menu_icon . "</p>\n";;
-     }
-
-
-    $hold = false; // dit is puur om de code te stoppen
-    if($hold){
-        $flex_checkbox_field = get_option('flex_settings_checkbox_field');
-        $args = array(
-            'post_type' => 'all'        
-        );
-        $all_posts = new WP_Query($args);
-        // echo '<pre>';
-        // var_dump($all_posts);
-        // echo '</pre>';
-    
-    
-        if($all_posts->have_posts()){
-    
-            $i = 0;
-    
-            while($all_posts->have_posts()){
-                $all_posts->the_post();
-                $post_id = get_the_ID();
-                $post_title = get_the_title();
-    
-                $i++;
-                ?>
-                <div>
-                    <input type="checkbox" id="option<?=$i ?>" name= "flex_settings_checkbox_field[<?=$post_id ?>]" value="<?= $post_id ?>" <?php checked( isset( $flex_checkbox_field[$post_id] ) ); ?> />
-                    <label for="option<?=$i ?>"><?=$post_title ?></label>
-                </div>
-                <?php
-            }
-        }
-    
+        $post_name = $post_type->name;
+        ?>
+        <div>
+        <input type='checkbox' id=<?= $post_name ?> name='flex_settings_checkbox_field[<?= $post_name ?>]' value='<?= $post_name ?>' <?php checked( isset( $flex_checkbox_field[$post_name] ) ); ?>>
+        <label for=<?= $post_name ?>>Custom Post Type name:  <?= $post_name ?>  
+        </div>
+        <?php
     }
 
-    ?>
 
-    <?php
-
-    var_dump($flex_checkbox_field);
 }
+//this setting uses all the items in flex_checkbox_field and returns them
+function return_location_settings(){
+    $locationSettingField = get_option('flex_settings_checkbox_field');
+    $locationSettings = array();
+    if($locationSettingField != null){
+        foreach($locationSettingField as $location){
+            $locationArray = array( 
+                array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => $location, // this is the instance of the location
+            ));
+    
+
+            array_push  ($locationSettings, $locationArray);
+        }
+    }
+
+    acf_add_local_field_group(array(
+        'key' => 'group_5bd180b0438cb',
+        'title' => 'Flexibele Content',
+        'fields' => array(
+            array(
+                'key' => 'field_60742b70143cb',
+                'label' => 'Flexibele Content',
+                'name' => 'flexibele_content',
+                'type' => 'flexible_content',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'acfe_flexible_advanced' => 0,
+                'layouts' => array(
+                    'layout_60742b7860cec' => array(
+                        'key' => 'layout_60742b7860cec',
+                        'name' => 'text_editor',
+                        'label' => 'Text ',
+                        'display' => 'block',
+                        'sub_fields' => array(
+                            array(
+                                'key' => 'field_60742c8a8ce69',
+                                'label' => 'Content',
+                                'name' => 'content',
+                                'type' => 'wysiwyg',
+                                'instructions' => '',
+                                'required' => 0,
+                                'conditional_logic' => 0,
+                                'wrapper' => array(
+                                    'width' => '',
+                                    'class' => '',
+                                    'id' => '',
+                                ),
+                                'default_value' => '',
+                                'tabs' => 'all',
+                                'toolbar' => 'full',
+                                'media_upload' => 1,
+                                'delay' => 0,
+                            ),
+                        ),
+                        'min' => '',
+                        'max' => '',
+                        'acfe_flexible_render_template' => false,
+                        'acfe_flexible_render_style' => false,
+                        'acfe_flexible_render_script' => false,
+                        'acfe_flexible_thumbnail' => false,
+                        'acfe_flexible_settings' => false,
+                        'acfe_flexible_settings_size' => 'medium',
+                        'acfe_flexible_modal_edit_size' => false,
+                        'acfe_flexible_category' => false,
+                    ),
+                    'layout_60745ff62b636' => array(
+                        'key' => 'layout_60745ff62b636',
+                        'name' => 'afbeelding',
+                        'label' => 'Afbeelding',
+                        'display' => 'block',
+                        'sub_fields' => array(
+                            array(
+                                'key' => 'field_607548882b637',
+                                'label' => 'Content',
+                                'name' => 'content',
+                                'type' => 'url',
+                                'instructions' => '',
+                                'required' => 0,
+                                'conditional_logic' => 0,
+                                'wrapper' => array(
+                                    'width' => '',
+                                    'class' => '',
+                                    'id' => '',
+                                ),
+                                'default_value' => '',
+                                'placeholder' => '',
+                            ),
+                        ),
+                        'min' => '',
+                        'max' => '',
+                        'acfe_flexible_render_template' => false,
+                        'acfe_flexible_render_style' => false,
+                        'acfe_flexible_render_script' => false,
+                        'acfe_flexible_thumbnail' => false,
+                        'acfe_flexible_settings' => false,
+                        'acfe_flexible_settings_size' => 'medium',
+                        'acfe_flexible_modal_edit_size' => false,
+                        'acfe_flexible_category' => false,
+                    ),
+                    'layout_607548972b638' => array(
+                        'key' => 'layout_607548972b638',
+                        'name' => 'quote',
+                        'label' => 'Quote',
+                        'display' => 'block',
+                        'sub_fields' => array(
+                            array(
+                                'key' => 'field_607548d32b639',
+                                'label' => 'Content',
+                                'name' => 'content',
+                                'type' => 'text',
+                                'instructions' => '',
+                                'required' => 0,
+                                'conditional_logic' => 0,
+                                'wrapper' => array(
+                                    'width' => '',
+                                    'class' => '',
+                                    'id' => '',
+                                ),
+                                'default_value' => '',
+                                'placeholder' => '',
+                                'prepend' => '',
+                                'append' => '',
+                                'maxlength' => '',
+                            ),
+                        ),
+                        'min' => '',
+                        'max' => '',
+                        'acfe_flexible_render_template' => false,
+                        'acfe_flexible_render_style' => false,
+                        'acfe_flexible_render_script' => false,
+                        'acfe_flexible_thumbnail' => false,
+                        'acfe_flexible_settings' => false,
+                        'acfe_flexible_settings_size' => 'medium',
+                        'acfe_flexible_modal_edit_size' => false,
+                        'acfe_flexible_category' => false,
+                    ),
+                    'layout_60812cbc803db' => array(
+                        'key' => 'layout_60812cbc803db',
+                        'name' => 'gallery',
+                        'label' => 'Gallery',
+                        'display' => 'block',
+                        'sub_fields' => array(
+                            array(
+                                'key' => 'field_60812ce2803dc',
+                                'label' => 'Content',
+                                'name' => 'content',
+                                'type' => 'gallery',
+                                'instructions' => '',
+                                'required' => 0,
+                                'conditional_logic' => 0,
+                                'wrapper' => array(
+                                    'width' => '',
+                                    'class' => '',
+                                    'id' => '',
+                                ),
+                                'return_format' => 'array',
+                                'preview_size' => 'medium',
+                                'insert' => 'append',
+                                'library' => 'all',
+                                'min' => '',
+                                'max' => '',
+                                'min_width' => '',
+                                'min_height' => '',
+                                'min_size' => '',
+                                'max_width' => '',
+                                'max_height' => '',
+                                'max_size' => '',
+                                'mime_types' => '',
+                            ),
+                        ),
+                        'min' => '',
+                        'max' => '',
+                        'acfe_flexible_render_template' => false,
+                        'acfe_flexible_render_style' => false,
+                        'acfe_flexible_render_script' => false,
+                        'acfe_flexible_thumbnail' => false,
+                        'acfe_flexible_settings' => false,
+                        'acfe_flexible_settings_size' => 'medium',
+                        'acfe_flexible_modal_edit_size' => false,
+                        'acfe_flexible_category' => false,
+                    ),
+                ),
+                'button_label' => 'Nieuwe regel',
+                'min' => '',
+                'max' => '',
+                'acfe_flexible_stylised_button' => false,
+                'acfe_flexible_hide_empty_message' => false,
+                'acfe_flexible_empty_message' => '',
+                'acfe_flexible_layouts_templates' => false,
+                'acfe_flexible_layouts_previews' => false,
+                'acfe_flexible_layouts_placeholder' => false,
+                'acfe_flexible_layouts_thumbnails' => false,
+                'acfe_flexible_layouts_settings' => false,
+                'acfe_flexible_disable_ajax_title' => false,
+                'acfe_flexible_layouts_ajax' => false,
+                'acfe_flexible_add_actions' => array(
+                ),
+                'acfe_flexible_remove_button' => array(
+                ),
+                'acfe_flexible_layouts_state' => false,
+                'acfe_flexible_modal_edit' => array(
+                    'acfe_flexible_modal_edit_enabled' => false,
+                    'acfe_flexible_modal_edit_size' => 'large',
+                ),
+                'acfe_flexible_modal' => array(
+                    'acfe_flexible_modal_enabled' => false,
+                    'acfe_flexible_modal_title' => false,
+                    'acfe_flexible_modal_size' => 'full',
+                    'acfe_flexible_modal_col' => '4',
+                    'acfe_flexible_modal_categories' => false,
+                ),
+            ),
+        ),
+        'location' => $locationSettings
+        ,
+        'menu_order' => 0,
+        'position' => 'acf_after_title',
+        'style' => 'default',
+        'label_placement' => 'left',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+        'acfe_display_title' => '',
+        'acfe_autosync' => '',
+        'acfe_form' => 0,
+        'acfe_meta' => '',
+        'acfe_note' => '',
+    ));
+}
+
+
+
+function return_location_group_settings(){
+    $location =  array(
+
+        array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'cases', // HIER MOET FUNCTIE KOMEN
+                ),
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'team', // HIER MOET FUNCTIE KOMEN
+                )
+            ),
+        );
+    
+ 
+    return $location;
+}
+
 
 
 ?>
